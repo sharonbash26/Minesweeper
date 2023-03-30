7c532de0f190f21fc8fca992a4e398c6b18a469c
sharonbash26 <45743653+sharonbash26@users.noreply.github.com>
2023-03-30 12:11:34 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -4,16 +4,18 @@ const BOMB = '💥'
 const FLAG = '🚩'
 var gBoard
 var gIsGameOn = false
+var gRoundNumber=0
+var gIsRightClick=false
+
 function onInit() {
     gBoard = createBoard(4, 4)
-
     renderBoard(gBoard, '.board-container')
-
+    gRoundNumber++
+ 
+    gBoard[0][2] = { minesAroundCount: 0, isShown: false, isMine: true, isMarked: false }
+    gBoard[1][2] = { minesAroundCount: 0, isShown: false, isMine: true, isMarked: false }
     gBoard = countMinesForAllCells(gBoard)
-    gBoard[0][2] = { minesAroundCount: 2, isShown: true, isMine: true, isMarked: true }
-    gBoard[1][2] = { minesAroundCount: 5, isShown: true, isMine: true, isMarked: true }
-
-    console.table(gBoard)
+    //console.table(gBoard)
 
     renderBoard(gBoard, '.board-container')
 
@@ -29,10 +31,7 @@ function createBoard(rows, cols) {
         //board[i][j] = (Math.random() > 0.5) ? LIFE : ''
         ///TODO RANDOM THIS AFTEER THIS
     }
-
-
-    return board;
-
+  return board;
 }
 
 
@@ -42,13 +41,9 @@ function countMinesForAllCells(board) {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
             var numOfNegs = countNegs(i, j, board)
-            if (!board[i][j].isMine) {
-                continue
-            } else {
+            if (board[i][j].isMine===false) {
                 newBoard[i][j] = { minesAroundCount: numOfNegs, isShown: false, isMine: false, isMarked: false }
-            }
-
-            //else if (board[i][j] === BOMB) newBoard[i][j] = ''
+            } 
         }
     }
     return newBoard
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//sharon bashh
 function renderBoard(mat, selector) {
     var cell
     var strHTML = '<table border="0"><tbody>'
@@ -7,10 +7,16 @@ function renderBoard(mat, selector) {
 
         strHTML += '<tr>'
         for (var j = 0; j < mat[0].length; j++) {
+            //debugger
             if (mat[i][j].isShown) {
+                //console.log('mat[i][j]',mat[i][j].isShown,'hh')
+
                 cell = mat[i][j].minesAroundCount
             } else {
+                //console.log('mat[i][j]',mat[i][j].isShown,'hh')
+
                 cell = '🟦'
+
             }
 
             const className = `cell cell-${i}-${j}`
@@ -18,7 +24,7 @@ function renderBoard(mat, selector) {
             // TODO I ADD THIS AND I NEED TO CHANGE THIS ACCRODING !!!!!/////
             strHTML += `<td class="${className}"
             data-i="${i}" data-j="${j}"
-            onclick="onCellClicked(this,${i},${j})">
+            onclick="onCellClicked(this,${i},${j})" onmousedown="onCellMouseDown(event,${i},${j})">
             ${cell}</td>`
             //////////////////////////////////////////////////////////////////
             //strHTML += `<td class="${className}">${cell}" onclick="onCellClicked(this,${i},${j})</td>`
@@ -30,20 +36,55 @@ function renderBoard(mat, selector) {
     const elContainer = document.querySelector(selector)
     elContainer.innerHTML = strHTML
 }
-function onCellClicked(elCell, cellI, cellJ) {
-    
-    var countBombs=gBoard[cellI][cellJ].minesAroundCount
-    var elCell = document.querySelector(`[data-i="${cellI}"][data-j="${cellJ}"]`)
-    if (gBoard[cellI][cellJ].isMine && gBoard[cellI][cellJ].isShown) {
-        console.log('tt')
-        elCell.innerText = BOMB
-    }else{
-        elCell.innerText=countBombs
-    }
-    if(gBoard[cellI][cellJ].isMark && gBoard[cellI][cellJ].isShown){
-        elCell.innerText=FLAG
+function onCellMouseDown(event, cellI, cellJ) {
+    // debugger
+    if (event.button === 2) { // right mouse button pressed
+        event.preventDefault();// prevent default behavior of right click
+        gIsRightClick = true
+        onCellClicked(event.target, cellI, cellJ);
+    } else {
+        gIsRightClick = false // reset gIsRightClick to false
+        onCellClicked(event.target, cellI, cellJ);
+        
     }
 }
+function onCellClicked(elCell, cellI, cellJ,) {
+  //  debugger
+    if (gIsRightClick) {
+        if (elCell.innerText === FLAG) {
+            elCell.innerText = '🟦'
+            gBoard[cellI][cellJ].isMark=false
+            gBoard[cellI][cellJ].isShown=false
+            console.log('new board')
+            console.table(gBoard)
+            return
+        }
+        elCell.innerText = FLAG
+        gBoard[cellI][cellJ].isMark=true
+        gBoard[cellI][cellJ].isShown=true
+     //   gBoard[cellI][cellJ].isShown = true
+    //    console.log('yy')
+        // console.log(gBoard[cellI][cellJ].isShown)
+        return
+    }
+
+    console.table(gBoard)
+    gIsRightClick = false
+    var countBombs = gBoard[cellI][cellJ].minesAroundCount
+    var elCell = document.querySelector(`[data-i="${cellI}"][data-j="${cellJ}"]`)
+    if (gBoard[cellI][cellJ].isMine && !gBoard[cellI][cellJ].isShown) {
+        console.log('tt')
+        elCell.innerText = BOMB
+    } else {
+        elCell.innerText = countBombs
+    }
+    if (gBoard[cellI][cellJ].isMark && gBoard[cellI][cellJ].isShown) {
+        elCell.innerText = FLAG
+    }
+
+}
+
+
 
 
 // location is an object like this - { i: 2, j: 7 }
@@ -62,7 +103,10 @@ function countNegs(rowIdx, colIdx, mat) {
         for (var j = colIdx - 1; j <= colIdx + 1; j++) {
             if (j < 0 || j >= mat[i].length) continue
             if (i === rowIdx && j === colIdx) continue
-            if (mat[i][j] === BOMB) negsCount++
+            if (mat[i][j].isMine) {
+                negsCount++
+            }
+            //console.log('i', i, 'j', j, 'negCount', negsCount)
         }
     }
     return negsCount
